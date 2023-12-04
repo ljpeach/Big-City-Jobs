7c72a1d6cb5be17a6853889cb20dd69a1e287b13
Liam Peachey <liamjpeachey@gmail.com>
2023-12-04 13:23:52 -0600

    Added new paths to main.jsx, added links to pages in header, added donation page with donation header

---
+++ client/src/pages/DonationPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import DonationCard from '../components/Donationcard'
+
+export default function DonationPage() {
+
+    return (
+        <div>
+            <DonationCard />
+        </div >
+    );
+}
