0bc1fd8be7bc1077ca09092457648e99b66ca4fa
Liam Peachey <liamjpeachey@gmail.com>
2023-12-04 13:50:57 -0600

    Started work on employer page

--- client/src/pages/EmployerPage.jsx
+++ client/src/pages/EmployerPage.jsx
@@ -1,5 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_EMPLOYER } from '../utils/queries';
 
 export default function EmployerPage() {
-	
+    const employerId = useParams(employerId);
+    const { loading, data } = useQuery(QUERY_EMPLOYER, { variables: { employerId } });
+    console.log(data);
+    const employer = data.employer;
+    return (
+        <div>
+            <section id='employerInfo'>
+                <h3>{employer.name}</h3>
+
+            </section>
+        </div>
+    );
 }
