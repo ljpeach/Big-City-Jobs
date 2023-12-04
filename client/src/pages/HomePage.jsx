d3c26d759ae72369bcfd587b0f5d18e03e3e4f07
Liam Peachey <liamjpeachey@gmail.com>
2023-12-04 10:45:00 -0600

    Got front end query working for user. Displays user data on homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,11 +3,26 @@ import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 import { useEffect } from 'react';
 export default function HomePage() {
-    // const { data } = useQuery(QUERY_USER);
+    const { loading, data } = useQuery(QUERY_USER);
+    const profile = data?.user || 'not working';
     useEffect(() => {
-        // console.log(data);
     }, [])
+    console.log(data);
     return (
-        <div>This is Home. Logged in: {Auth.loggedIn().toString()}</div>
+        <>
+            <div>This is Home. Logged in: {Auth.loggedIn().toString()}</div>
+            {
+                Auth.loggedIn() ? (
+                    <ul>
+                        <li>First: {data?.user.firstName}</li>
+                        <li>Last: {data?.user.lastName}</li>
+                        <li>Email: {data?.user.email}</li>
+                        <li>ID: {data?.user._id}</li>
+                    </ul>
+                ) : (
+                    <div>Not signed in.</div>
+                )
+            }
+        </>
     );
 }
