387d98a4728ce90437c99e4518a06ff53a74aa90
Kevin Patto <kevp2010@gmail.com>
2023-12-05 11:29:31 -0600

    links justify to right, logout displays when logged in

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 
 const Header = () => {
   return (
-    <header className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <header className="navbar navbar-expand-lg navbar-dark bg-dark p-2 d-flex justify-content-between">
       <Link className="navbar-brand" to="/">
         Your Logo
       </Link>
 
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="loginsignup">Log In/Sign Up</Link>
-          </li>
-          <li className="nav-item">
-            <Link className='nav-link' to='donate'>Donate</Link>
-          </li>
-        </ul>
-      </div>
+      <ul className="navbar-nav">
+        {
+          Auth.loggedIn() ? (
+            <li className="nav-item">
+              <Link className="nav-link" to="">Logout</Link>
+            </li>
+          ) : (
+            <li className="nav-item">
+              <Link className="nav-link" to="loginsignup">Log In/Sign Up</Link>
+            </li>
+          )
+        }
+        <li className="nav-item">
+          <Link className='nav-link' to='donate'>Donate</Link>
+        </li>
+      </ul>
     </header>
   );
 };
