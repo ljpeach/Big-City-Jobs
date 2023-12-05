4a03c4a760e99b40961f3fc969ac837884136d0b
Kevin Patto <kevp2010@gmail.com>
2023-12-05 11:47:53 -0600

    footer now floats to bottom

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,7 +10,7 @@ const collaborators = [
 
 const Footer = () => {
 	return (
-		<footer class="d-flex justify-content-center py-1 my-1 text-body-secondary smaller-text position-absolute bottom-0 start-50 translate-middle-x">
+		<footer class="d-flex justify-content-center py-1 mt-auto text-body-secondary smaller-text">
 			<span class="px-2">
 				Visit our GitHubs:
 			</span>
