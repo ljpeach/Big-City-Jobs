22843a204834f7d3d5e9ee30e0050ac039484fc4
Kevin Patto <kevp2010@gmail.com>
2023-12-06 12:38:58 -0600

    added an s

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   return (
     <header className="navbar navbar-expand-lg navbar-dark bg-dark p-2 d-flex justify-content-between">
       <Link className="navbar-brand" to="/">
-        Big City Job
+        Big City Jobs
       </Link>
 
       <ul className="navbar-nav">
