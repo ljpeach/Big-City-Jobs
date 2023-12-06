a921be5e040b95aa84053998b1b00974a2a27f3c
Kevin Patto <kevp2010@gmail.com>
2023-12-06 13:21:30 -0600

    actually works now

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,49 +1,27 @@
-import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+import JobList from '../components/JobList';
 
 export default function ProfilePage() {
-    return (
-        <div>
-            <h1>First and Last Name</h1>
-            <p>User description</p>
-            <p>Location: User location</p>
-        </div>,
-        
-    <div>
-    {showTitle && <h3>{title}</h3>}
-    {jobPostings &&
-      jobPostings.map((job) => (
-        <div key={job._id} className="card mb-3">
-          <h4 className="card-header bg-primary text-light p-2 m-0">
-            {/* Add a check to see if the job is saved by the user */}
-            {userSavedJobs.includes(job._id) && (
-              <span className="text-warning">Saved by you!</span>
-            )}
-            {showEmployer ? (
-              <Link className="text-light" to={`/employers/${job.employer}`}>
-                {job.employer} <br />
-                <span style={{ fontSize: '1rem' }}>
-                  posted this job on {job.postedDate}
-                </span>
-              </Link>
-            ) : (
-              <>
-                <span style={{ fontSize: '1rem' }}>
-                  This job was posted on {job.postedDate}
-                </span>
-              </>
-            )}
-          </h4>
-          <div className="card-body bg-light p-2">
-            <p>{job.details}</p>
-          </div>
-          <Link
-            className="btn btn-primary btn-block btn-squared"
-            to={`/jobs/${job._id}`}
-          >
-            View details and apply for this job.
-          </Link>
-        </div>
-      ))}
-  </div>
-);
+	const { loading, data, error } = useQuery(QUERY_USER);
+	console.log(data);
+
+	const jobs = data?.user.savedJobs;
+
+	return loading ? (<div>loading</div>) : (
+		<div className='d-flex flex-row'>
+			<section id='jobs' className='col-12 col-md-9 p-2 mx-auto'>
+				{jobs.length ? (
+					<JobList
+						jobPostings={jobs}
+						title="Your Saved Jobs"
+						showTitle={true}
+						showEmployer={true}
+					/>
+				) : (
+					<h3 className="text-center">No one has posted any jobs.</h3>
+				)}
+			</section>
+		</div >
+	);
 }
